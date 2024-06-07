9da932a9cf57c34343a5cea1f6561ba1ef1ff43b
Alperen Alperen <aalperen@icloud.com>
2024-06-07 19:48:42 +0300

    event repo and bloc

---
+++ lib/src/blocs/calendar/calendar_bloc.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'package:tobeto/src/domain/repositories/calendar_repository.dart';
+
+import '../../models/calendar_model.dart';
+
+part 'calendar_event.dart';
+part 'calendar_state.dart';
+
+class CalendarBloc extends Bloc<CalendarEvent, CalendarState> {
+  final CalendarRepository _calendarRepository;
+
+  CalendarBloc(
+    this._calendarRepository,
+  ) : super(CalendarInitial()) {
+    // eventları çekme
+    on<FetchAllEvents>((event, emit) async {
+      emit(CalendarLoading());
+      try {
+        final events = await _calendarRepository.fetchEventsFromFirestore();
+        emit(CalendarLoaded(events: events));
+      } catch (_) {
+        emit(CalendarFailed());
+      }
+    });
+
+    // event ekleme
+    on<AddEvent>((event, emit) async {
+      emit(CalendarLoading());
+      try {
+        await _calendarRepository.addEvent(eventModel: event.eventModel);
+        emit(CalendarSuccess());
+      } catch (_) {
+        emit(CalendarFailed());
+      }
+    });
+
+    // event güncelleme
+    on<UpdateEvent>((event, emit) async {
+      emit(CalendarLoading());
+      try {
+        await _calendarRepository.updateEvent(eventModel: event.eventModel);
+        emit(CalendarSuccess());
+      } catch (_) {
+        emit(CalendarFailed());
+      }
+    });
+
+    // event silme
+    on<UpdateEvent>((event, emit) async {
+      emit(CalendarLoading());
+      try {
+        await _calendarRepository.deleteEvent(eventModel: event.eventModel);
+        emit(CalendarSuccess());
+      } catch (_) {
+        emit(CalendarFailed());
+      }
+    });
+  }
+}
---
+++ lib/src/domain/repositories/calendar_repository.dart
@@ -0,0 +1,65 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:tobeto/src/common/constants/firebase_constants.dart';
+
+import '../../models/calendar_model.dart';
+
+class CalendarRepository {
+  final FirebaseFirestore _firebaseFirestore = FirebaseFirestore.instance;
+
+  CollectionReference get _events =>
+      _firebaseFirestore.collection(FirebaseConstants.events);
+
+  Future<List<EventModel>> fetchEventsFromFirestore() async {
+    List<EventModel> events = [];
+    var querySnapshot = await _events.get();
+    for (var doc in querySnapshot.docs) {
+      events.add(EventModel.fromMap(doc.data() as Map<String, dynamic>));
+    }
+    return events;
+  }
+
+  Future<String> addEvent({
+    required EventModel eventModel,
+  }) async {
+    String result = '';
+
+    try {
+      await _events.add(eventModel.toMap());
+      result = 'success';
+    } catch (error) {
+      result = error.toString();
+    }
+
+    return result;
+  }
+
+  Future<String> updateEvent({
+    required EventModel eventModel,
+  }) async {
+    String result = '';
+
+    try {
+      await _events.doc(eventModel.id).delete();
+      result = 'success';
+    } catch (error) {
+      result = error.toString();
+    }
+
+    return result;
+  }
+
+  Future<String> deleteEvent({
+    required EventModel eventModel,
+  }) async {
+    String result = '';
+
+    try {
+      await _events.doc(eventModel.id).delete();
+      result = 'success';
+    } catch (error) {
+      result = error.toString();
+    }
+
+    return result;
+  }
+}
