dcebfe674acce33b147c25f4587baa820b41940e
Alperen Alperen <aalperen@icloud.com>
2024-06-14 14:11:06 +0300

    main update

--- lib/app_view.dart
+++ lib/app_view.dart
@@ -5,6 +5,8 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:tobeto/src/blocs/auth/auth_bloc.dart';
 import 'package:tobeto/src/common/router/app_router.dart';
 import 'package:tobeto/src/domain/repositories/auth_repository.dart';
+import 'package:tobeto/src/presentation/screens/home/home_screen.dart';
+import 'package:tobeto/src/presentation/screens/platform/platform_screen.dart';
 
 class MainApp extends StatefulWidget {
   const MainApp({super.key});
@@ -54,10 +56,26 @@ class _MainAppState extends State<MainApp> {
         // initialRoute: initScreen == 0 || initScreen == null
         //     ? AppRouteNames.onboardingRoute
         //     : AppRouteNames.platformScreenRoute,
-        initialRoute: FirebaseAuth.instance.currentUser == null
-            ? AppRouteNames.homeRoute
-            : AppRouteNames.platformScreenRoute,
-        // home: NavigationBarWidget(),
+        // initialRoute: FirebaseAuth.instance.currentUser == null
+        //     ? AppRouteNames.homeRoute
+        //     : AppRouteNames.platformScreenRoute,
+
+        home: BlocBuilder<AuthBloc, AuthState>(
+          builder: (context, state) {
+            if (state is AuthInitial || state is AuthLoading) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            } else if (state is Authenticated) {
+              return const PlatformScreen();
+            } else if (state is Unauthenticated) {
+              return const HomeScreen();
+            }
+            return const Center(
+              child: Text('Hata!'),
+            );
+          },
+        ),
       ),
     );
   }
